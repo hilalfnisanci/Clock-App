af277c1e1031907ecc4bf5de56efaed0e930407d
hilal nisanci <hilalfnisanci@gmail.com>
2023-12-19 14:19:58 +0300

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Hilal on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
